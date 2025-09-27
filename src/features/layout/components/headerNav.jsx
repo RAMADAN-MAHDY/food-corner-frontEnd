6e7e1dbfc768e400ec4ad5c0f28a8e5cc8d91d18
dina abaza <DinaNabilAbaza@gmail.com>
2025-09-27 20:45:14 +0200

    edit

--- src/features/layout/components/headerNav.jsx
+++ src/features/layout/components/headerNav.jsx
@@ -3,7 +3,7 @@ import { FaFacebook, FaInstagram } from 'react-icons/fa';
 
 export default function HeaderNav() {
   return (
-    <div className="w-full h-7 flex justify-between items-center bg-pink-900 text-white font-bold px-2">
+    <div className="w-full h-10 flex justify-between items-center bg-pink-900 text-white font-bold px-2">
       <div className="flex gap-4 justify-center items-center w-1/3 text-xs sm:text-sm">
         <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
           <FaFacebook className="text-white text-lg" />
